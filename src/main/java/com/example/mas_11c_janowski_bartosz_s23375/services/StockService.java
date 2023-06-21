65f5e9a1005cca4d282cc5a691dfcba601a95d4e
Bartosz Janowski <detnotatka@gmail.com>
2023-06-21 21:15:51 +0200

    Final commit v1.03

--- src/main/java/com/example/mas_11c_janowski_bartosz_s23375/services/StockService.java
+++ src/main/java/com/example/mas_11c_janowski_bartosz_s23375/services/StockService.java
@@ -15,6 +15,12 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
+    /**
+     * Updates the stock based on the provided stock object. If the stock doesn't exist, a new stock is created.
+     *
+     * @param stock The stock object to update or insert.
+     * @return True if the stock was successfully updated or inserted, false otherwise.
+     */
     public boolean updateStock(Stock stock) {
         Optional<Stock> downloadedStock = fetchStockForUpdate(stock);
 
@@ -28,6 +34,12 @@ public class StockService {
         }
     }
 
+    /**
+     * Inserts a new stock into the database if the stock's stock number, storage room, and instrument are not null.
+     *
+     * @param stock The stock object to insert.
+     * @return True if the stock was successfully inserted, false otherwise.
+     */
     private boolean insertNewStock(Stock stock) {
         if(stock.getStockNumber() != null && stock.getStorageRoom() != null && stock.getInstrument() != null) {
             stockRepository.save(stock);
@@ -37,6 +49,12 @@ public class StockService {
         return false;
     }
 
+    /**
+     * Updates an existing stock in the database if the stock's stock number, storage room, and instrument are not null.
+     *
+     * @param stock The stock object to update.
+     * @return True if the stock was successfully updated, false otherwise.
+     */
     private boolean updateExistingStock(Stock stock) {
         if(stock.getStockNumber() != null && stock.getStorageRoom() != null && stock.getInstrument() != null) {
             stockRepository.save(stock);
@@ -46,6 +64,12 @@ public class StockService {
         return false;
     }
 
+    /**
+     * Fetches the stock object for update based on the storage room and instrument.
+     *
+     * @param stock The stock object containing the storage room and instrument information.
+     * @return Optional containing the stock object if found, or empty if not found.
+     */
     private Optional<Stock> fetchStockForUpdate(Stock stock) {
         return stockRepository.findStockByStorageRoomAndInstrument(stock.getStorageRoom(), stock.getInstrument());
     }
--- src/test/java/com/example/mas_11c_janowski_bartosz_s23375/models/AssociationsInheritancesTest.java
+++ src/test/java/com/example/mas_11c_janowski_bartosz_s23375/models/AssociationsInheritancesTest.java
@@ -24,7 +24,6 @@ import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
